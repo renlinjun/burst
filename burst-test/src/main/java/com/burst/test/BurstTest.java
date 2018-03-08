045a1e4e510c27085c1578157ec39612e0a4dc75
renlinjun <renlinjun89@163.com>
2018-03-08 09:51:48 +0800

    first commit

---
+++ burst-test/src/main/java/com/burst/test/BurstTest.java
@@ -0,0 +1,12 @@
+package com.burst.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurstTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurstTest.class);
+	}
+}
